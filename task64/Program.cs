9aebd1c163fddb39565d02deb57528bf165e007f
Semilael <ariana-ses97@yandex.ru>
2022-09-04 16:05:47 +0300

    Task added

---
+++ task64/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (m < n)
+{
+    Console.Write("Диапазон введенных чисел: ");
+    for(int i = m; i <= n; i++)
+    {
+        Console.Write(i + " ");
+    }
+}
+if (m > n)
+{
+    Console.Write("Диапазон введенных чисел: ");
+    for (int i = m ; i >= n ; i--)
+    {
+        Console.Write(i + " ");
+    } 
+}
+else
+{
+    Console.WriteLine("Вы ввели неверный диапазон. Попробуйте снова.");
+}
---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (m < n)
+{
+    int sum = 0;
+    for(int i = m; i <= n; i++)
+    {
+        sum += i;
+    }
+    Console.Write($"Сумма диапазона введенных чисел: {sum}");
+}
+else
+{
+    Console.WriteLine("Вы ввели неверный диапазон. Попробуйте снова.");
+}
